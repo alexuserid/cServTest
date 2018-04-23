3b1b695442a2bf2ae306adba9dc0cb13daf9340d
A <alexuserid@gmail.com>
2018-04-23 11:38:56 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,33 +6,47 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 var (
-	address = flag.String("l", "http://localhost:8080", "Server address for test")
+	address   = flag.String("l", "http://localhost:8080", "Server address for test")
+	checkTime = flag.Duration("t", 200*time.Millisecond, "Test durationi in millisecond")
 )
 
 func main() {
 	flag.Parse()
 	url := *address
 
-	for i, v := range mass {
-		resp, err := http.Get(url + "?s=" + v.s)
-		if err != nil {
-			log.Fatalf("http.Get: %v", err)
-		}
-		bytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalf("ioutil.ReadAll: %v", err)
-		}
-		resp.Body.Close()
+	timeout := time.After(*checkTime)
+	for i := 0; ; i++ {
 
-		if string(bytes) != v.n+"\n" {
-			fmt.Printf(`Wrong answer. Test %d: %s.
-			Server answer is: %sRight answer is: %s
-			`, i, v.s, string(bytes), v.n)
+		select {
+		case <-timeout:
+			fmt.Printf("Timeout. Test was repited %d times. Duration: %v.", i, checkTime)
 			return
+		default:
+			for j, v := range mass {
+				resp, err := http.Get(url + "?s=" + v.s)
+				if err != nil {
+					log.Fatalf("http.Get: %v", err)
+				}
+				bytes, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					log.Fatalf("ioutil.ReadAll: %v", err)
+				}
+				resp.Body.Close()
+
+				ans := strings.TrimSpace(string(bytes))
+				if string(ans) != v.n {
+					fmt.Printf(`Wrong answer. Test %d: %s.
+Server answer is: %q
+Right answer is: %s
+`, j, v.s, string(bytes), v.n)
+					return
+				}
+			}
 		}
 	}
-	fmt.Println("Success!")
 }
